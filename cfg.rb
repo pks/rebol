b0bff4f48b2de88560199be09e5a29feecaa267c
Patrick Simianer <simianer@cl.uni-heidelberg.de>
2014-04-14 16:05:40 +0200

    smt-semparse

--- cfg.rb
+++ cfg.rb
@@ -1,12 +1,12 @@
 # edit here to change the parser
-SMT_SEMPARSE = 'python /workspace/grounded/smt-semparse-cp/decode_sentence.py /workspace/grounded/smt-semparse-cp/working/full_dataset'
-# this is the 'fixed' version of eval.pl
-EVAL_PL = '/workspace/grounded/wasp-1.0/data/geo-funql/eval/eval.pl'
+SMT_SEMPARSE = 'python /path/to/decode_sentence.py/of/smt-semparse /path/to/smt-semparse/workdir'
+# this should be a 'fixed' (one that doesn't abbreviate its output) version of eval.pl
+EVAL_PL = '/path/to/wasp-1.0/data/geo-funql/eval/eval.pl'
 # set to true to ignore zombie eval.pl procs
 ACCEPT_ZOMBIES = true
-TIMEOUT=60
+TIMEOUT = 60
 # cdec binary
-CDEC_BIN = '/toolbox/cdec-dtrain/decoder/cdec'
+CDEC_BIN = '/path/to/cdec'
 # memcached has to be running
-$cache = Memcached.new('localhost:11211')
+$cache = Memcached.new('localhost:31337')
 
